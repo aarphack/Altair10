4893b49f572411964e24cf2e311eb5ac90f452a9
Gregg Vesonder <vesonder@mac.com>
2017-07-09 13:45:12 -0400

    Initial commit

---
+++ Altair10_main.py
@@ -0,0 +1,23 @@
+#
+# okay i now have the real pycharm
+# begin porting my evolution of evolvability stuff
+# 6 July 2017
+
+# start with street programming and evolve into pythonic
+#
+
+# may turn this into a main object but not now.
+#
+
+
+#
+# first the constants, I will change this for the better later
+#
+
+cullnumber = 0
+DEBUG = 0
+inversionOn = True
+randomRun = True
+inversionRate = 0.0005
+numGenerations = 1000000000
+
